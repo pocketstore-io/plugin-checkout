3ac2cf503f3bb062a6a9645c8ffcfe63a318c05f
jonathan-martz <github@jmartz.de>
2025-11-19 21:40:38 +0100

    update paypal

--- components/checkout/payment/Paypal.vue
+++ components/checkout/payment/Paypal.vue
@@ -1,86 +1,85 @@
 <template>
-  <div>
-    <div id="paypal-button-container" class="mt-3" />
-    <button v-if="locked" class="btn btn-secondary w-full mt-3" @click="resetLock()">
-      <Fa :icon="faTrash" /> <span>Reset PayPal Payment</span>
-      <Fa :icon="faTrash" />
-    </button>
+  <div v-if="!paymentDetails.id">
+    <div id="paypal-button-container" class="mt-3"/>
+  </div>
+  <div v-else>
+    <section class="alert alert-success text-white mb-3 block text-center">
+      {{$t('alert.paypal.success')}}
+    </section>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { useLocalStorage } from "@vueuse/core";
-import { loadScript } from "@paypal/paypal-js";
-import { cartToPurchaseUnits } from "~/utils/paypal";
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import {useLocalStorage} from "@vueuse/core";
+import {loadScript} from "@paypal/paypal-js";
+import {cartToPurchaseUnits} from "~/utils/paypal";
+import {faTrash} from "@fortawesome/free-solid-svg-icons";
 import config from '~/pocketstore.json'
 
-const { locked } = defineProps({
-    locked: {
-        type: Boolean,
-        required: false,
-        default: () => {
-            return false;
-        },
+const {locked} = defineProps({
+  locked: {
+    type: Boolean,
+    required: false,
+    default: () => {
+      return false;
     },
+  },
 });
 
 const paymentMethod = useLocalStorage("paymentMethod", "vorkasse", {});
 const checkoutStep = useLocalStorage("checkoutStep", "cart", {});
+const paymentDetails = ref({});
 
-const resetLock = () => {};
+const resetLock = () => {
+};
 
 const initPaypal = () => {
-    loadScript({
-        "client-id": config.payment.paypal.id,
-        currency: config.payment.paypal.currency,
-    })
-        .then((paypal) => {
-            paypal
-                .Buttons({
-                    createOrder: (data, actions) => {
-                        // Directly create an order with the amount, no server needed
-                        return actions.order.create({
-                            purchase_units: cartToPurchaseUnits(),
-                        });
-                    },
-                    onApprove: (data, actions) =>
-                        actions.order.capture().then((details) => {
-                            alert(
-                                "Transaction completed by " +
-                                    details.payer.name.given_name,
-                            );
-                            console.log(details); // You can log or handle the transaction details here
-                        }),
-                    onError: (err) => {
-                        console.error(
-                            "An error occurred during the transaction",
-                            err,
-                        );
-                    },
-                })
-                .render("#paypal-button-container");
-        })
-        .catch((err) => {
-            console.error("failed to load the PayPal JS SDK script", err);
-        });
+  loadScript({
+    "client-id": config.payment.paypal.id,
+    currency: config.payment.paypal.currency,
+  })
+      .then((paypal) => {
+        paypal
+            .Buttons({
+              createOrder: (data, actions) => {
+                // Directly create an order with the amount, no server needed
+                return actions.order.create({
+                  purchase_units: cartToPurchaseUnits(),
+                });
+              },
+              onApprove: (data, actions) =>
+                  actions.order.capture().then((details) => {
+                    paymentDetails.value = details;
+                  }),
+              onError: (err) => {
+                console.error(
+                    "An error occurred during the transaction",
+                    err,
+                );
+              },
+            })
+            .render("#paypal-button-container");
+      })
+      .catch((err) => {
+        console.error("failed to load the PayPal JS SDK script", err);
+      });
 };
 
 onMounted(() => {
-    if (paymentMethod.value == "paypal" && checkoutStep.value == "payment") {
-        initPaypal();
-    }
+  if (paymentMethod.value == "paypal" && checkoutStep.value == "payment") {
+    initPaypal();
+  }
 });
 
 watch(paymentMethod, (value) => {
-    if (value === "paypal") {
-        initPaypal();
-    }
+  if (value === "paypal") {
+    initPaypal();
+  }
 });
 
 watch(checkoutStep, (value) => {
-    if (paymentMethod.value == "paypal" && value == "payment") {
-        initPaypal();
-    }
+  if (paymentMethod.value == "paypal" && value == "payment") {
+    initPaypal();
+  }
 });
 </script>
