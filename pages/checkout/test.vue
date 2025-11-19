0f15345df935096e40e2345262485a426d2a96d8
jonathan-martz <github@jmartz.de>
2025-11-19 20:50:41 +0100

    add component and pages from storefront

--- pages/checkout/test.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <section class="bg-white xl:mx-auto mx-3 px-3 py-3 max-w-6xl">
-    Test Page for Checkout Plugin from Pocketstore.io
-  </section>
-</template>
-
-<script setup lang="ts"></script>
